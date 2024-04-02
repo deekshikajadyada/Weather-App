94820c800372350763a0738604182f0b8915f5b2
deekshikajadyada00 <deekshikajadyada0000@gmail.com>
2024-04-02 16:26:50 +0530

    Weather-wisepod

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useStateContext } from './Components/Context';
 import './App.css';
 
 function App() {
-  const [input, setInput] = useState(''); // Changed variable name to camelCase
+  const [input, setInput] = useState(''); 
   const { weather, thisLocation, values, place, setPlace } = useStateContext();
 
   const submitCity = () => {
